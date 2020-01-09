b22886084144a232a79d6b4949d1084e483a8c0c
lisa-h69 <55873930+lisa-h69@users.noreply.github.com>
2020-01-09 22:41:55 +0100

    Add files via upload

---
+++ projet_climat_sandra_lisa/src/projet_climat_sandra_lisa/interface_import.java
@@ -0,0 +1,8 @@
+package projet_climat_sandra_lisa;
+import javax.swing.JFrame;
+
+public class interface_import {
+  public static void main(String[] args){       
+	  Fenetre fen = new Fenetre();
+  }       
+}
