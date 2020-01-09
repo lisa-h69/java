69ec6a7279fcab660d582d492b70b3cc5b7a93da
lisa-h69 <55873930+lisa-h69@users.noreply.github.com>
2020-01-10 00:05:18 +0100

    Add files via upload

---
+++ projet_climat_sandra_lisa/src/projet_climat_sandra_lisa/Initialisation.java
@@ -0,0 +1,7 @@
+package projet_climat_sandra_lisa;
+public class Initialisation {
+	  public static void main(String[] args){      
+			Interface_import Interf = new Interface_import();
+	  }       
+
+}
